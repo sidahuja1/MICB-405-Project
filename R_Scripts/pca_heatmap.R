0d08a700c3d9a60fc11516ae7e036480b998d859
sidahuja1 <97431603+sidahuja1@users.noreply.github.com>
2024-12-06 13:03:06 -0800

    cleaning code in pca_heatmap.R

--- R_Scripts/pca_heatmap.R
+++ R_Scripts/pca_heatmap.R
@@ -5,29 +5,22 @@ library(RColorBrewer)
 library(ggrepel)
 library(gridExtra)
 
-# Load files into R
+# loading in the DESeq object
+dds <- readRDS("dds.rds")
 
-dds_matrix <- DESeqDataSetFromMatrix(countData = dat_matrix,
-                                     colData = metadata,
-                                     design = ~Timepoint)
+### PCA ###
 
-dds_matrix
-
-dds_matrix$condition <- relevel(dds_matrix$Timepoint, ref = "0_hour")
-
-# Check the levels of dds_matrix$condition
-levels(dds_matrix$Timepoint)
-dds <- DESeq(dds_matrix)
-saveRDS(dds, "dds.rds")
-
+# Regularized log transformations for PCA
 rld <- rlog(dds)
 
-# Generate a PCA plot with DESeq2's plotPCA function
+# Generating PCA data
 pcaData <- plotPCA(rld, intgroup=c("Timepoint"), returnData = TRUE)
 pcaData
 
+# Extracting the variation percents from the PCA object
 percentVar <- round(100 * attr(pcaData, "percentVar"))
 
+# PCA Plot
 pcaData %>% 
   ggplot(aes(x = PC1, y = PC2, colour = Timepoint)) +
   geom_point(size = 3) +
@@ -42,25 +35,29 @@ pcaData %>%
     panel.border = element_rect(linewidth = 2)) +
   labs(colour = "Timepoint")
 
+### HEATMAP ###
 
+# Removing ncRNA genes from dds object
 ncrna_genes <- grepl("NCRNA", rownames(dds), ignore.case = F)
 
-# Remove these genes from the dataset
 dds_filtered <- dds[!ncrna_genes, ]
 
+# Removing SPOM_ prefix from gene names
 rownames(dds_filtered) <- gsub("SPOM_", "", rownames(dds_filtered))
 
-# Perform log transformation on our count data
+# Perform log transformation on filtered count data
 rld_1 <- rlog(dds_filtered)
-# vsd <- vst(dds_filtered)
 rld_mat <- assay(rld_1)
 
+# Selecting top 20 most variable genes
 select <- head(order(rowVars(rld_mat), decreasing = TRUE), 20)
 selected_genes <- assay(rld_1[select,])
 selected_genes_names <- rownames(selected_genes)
 
+# Loading in gene id names from PomBase
 pom_genes <- read_tsv('data/gene_IDs_names.tsv', skip = 1)
 
+# Converting gene IDs to gene names
 filtered_pom_genes <- pom_genes[tolower(pom_genes$gene_systematic_id) %in% tolower(selected_genes_names), ]
 
 filtered_pom_genes_vec <- ifelse(is.na(filtered_pom_genes$gene_name), 
@@ -69,10 +66,13 @@ filtered_pom_genes_vec <- ifelse(is.na(filtered_pom_genes$gene_name),
 
 rownames(selected_genes) <- filtered_pom_genes_vec
 
+# Data frame of timepoint names for heatmap
 df <- as.data.frame(colData(dds_filtered)['Timepoint'])
 
+# Getting gene names for plot
 rownames(df) <- colnames(rld_1)
 
+# Generating heatmap
 pheatmap(selected_genes, scale = "row", cluster_rows=T, show_rownames=T,
          cluster_cols=F, annotation_col=df)
 
