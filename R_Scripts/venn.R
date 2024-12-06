022dbcf446a331549066c9463bbe58893ca2077b
sidahuja1 <97431603+sidahuja1@users.noreply.github.com>
2024-12-06 13:06:42 -0800

    cleaning code in venn.R

--- R_Scripts/venn.R
+++ R_Scripts/venn.R
@@ -6,30 +6,36 @@ library(ggrepel)
 library(gridExtra)
 library(VennDiagram)
 
-# Using dds from the data_load R file
+# loading in the DESeq object
+dds <- readRDS("dds.rds")
 
+# Filtering out ncRNA genes
 ncrna_genes <- grepl("NCRNA", rownames(dds), ignore.case = F)
 
-# Remove these genes from the dataset
 dds_filtered <- dds[!ncrna_genes, ]
 
+# Removing SPOM_ prefix from gene IDs
 rownames(dds_filtered) <- gsub("SPOM_", "", rownames(dds_filtered))
 
+# Pairwise comparisons (DGE)
 res_0_vs_12 <- results(dds_filtered, contrast=c("Timepoint", "0_hour", "12_hour"))
 res_0_vs_24 <- results(dds_filtered, contrast=c("Timepoint", "0_hour", "24_hour"))
 res_0_vs_36 <- results(dds_filtered, contrast=c("Timepoint", "0_hour", "36_hour"))
 res_0_vs_48 <- results(dds_filtered, contrast=c("Timepoint", "0_hour", "48_hour"))
 
+# Function to filter over expressed genes in the 0 hour vs other timepoints
 filter_genes <- function(res) {
   res_filtered <- res[!is.na(res$padj) & res$padj < 0.05 & res$log2FoldChange >= 2, ]
   return(rownames(res_filtered))
 }
 
+# Running function
 overexpressed_0_vs_12 <- filter_genes(res_0_vs_12)
 overexpressed_0_vs_24 <- filter_genes(res_0_vs_24)
 overexpressed_0_vs_36 <- filter_genes(res_0_vs_36)
 overexpressed_0_vs_48 <- filter_genes(res_0_vs_48)
 
+# Combining the genes
 gene_lists <- list(
   `0_vs_12` = overexpressed_0_vs_12,
   `0_vs_24` = overexpressed_0_vs_24,
@@ -37,8 +43,10 @@ gene_lists <- list(
   `0_vs_48` = overexpressed_0_vs_48
 )
 
+# Selecting color pallete
 myCol <- brewer.pal(4, "Set1")
 
+# Creating venn diagram
 venn.plot <- draw.quad.venn(
   area1 = length(overexpressed_0_vs_12),
   area2 = length(overexpressed_0_vs_24),
@@ -67,18 +75,19 @@ venn.plot <- draw.quad.venn(
 )
 
 
-
-
+# Function to filter under expressed genes in the 0 hour vs other timepoints
 filter_genes <- function(res) {
   res_filtered <- res[!is.na(res$padj) & res$padj < 0.05 & res$log2FoldChange > -2, ]
   return(rownames(res_filtered))
 }
 
+# Running function
 overexpressed_0_vs_12 <- filter_genes(res_0_vs_12)
 overexpressed_0_vs_24 <- filter_genes(res_0_vs_24)
 overexpressed_0_vs_36 <- filter_genes(res_0_vs_36)
 overexpressed_0_vs_48 <- filter_genes(res_0_vs_48)
 
+# Combining the genes
 gene_lists <- list(
   `0_vs_12` = overexpressed_0_vs_12,
   `0_vs_24` = overexpressed_0_vs_24,
@@ -86,7 +95,7 @@ gene_lists <- list(
   `0_vs_48` = overexpressed_0_vs_48
 )
 
-
+# Creating venn diagram
 venn.plot <- draw.quad.venn(
   area1 = length(overexpressed_0_vs_12),
   area2 = length(overexpressed_0_vs_24),
